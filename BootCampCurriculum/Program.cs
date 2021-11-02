2e56159c74743cf07b7cd504a907266e97fc365a
Seth-Race <sethrace@gmail.com>
2021-11-02 14:56:37 -0400

    Initial Commit

---
+++ BootCampCurriculum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BootCampCurriculum {
+    class Program {
+      
+
+       
+
+        static void Main(string[] args) 
+        {
+
+            var gitgithub = new Topic();
+            
+            gitgithub.Name = "Git/Github";
+            gitgithub.Category = "SCM";
+            gitgithub.Difficulty = 2;
+            gitgithub.Print();
+
+            var sqlserver = new Topic() {
+                Name = "SQL Server",
+                Category = "Database",
+                Difficulty = 3
+            };
+            sqlserver.Print(); 
+            
+        }
+    }
+}
+
